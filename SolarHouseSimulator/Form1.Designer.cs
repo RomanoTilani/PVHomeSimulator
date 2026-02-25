5ba8454e8f16287635e92870de6e57e0955a3145
rt <roman.thiel@web.de>
2026-02-25 16:32:13 +0100

    Added sim for bat

--- SolarHouseSimulator/Form1.Designer.cs
+++ SolarHouseSimulator/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.formsPlot = new ScottPlot.WinForms.FormsPlot();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonLoadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formsPlot
+            // 
+            this.formsPlot.DisplayScale = 0F;
+            this.formsPlot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot.Location = new System.Drawing.Point(0, 0);
+            this.formsPlot.Name = "formsPlot";
+            this.formsPlot.Size = new System.Drawing.Size(977, 835);
+            this.formsPlot.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonLoadData);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.formsPlot);
+            this.splitContainer1.Size = new System.Drawing.Size(1471, 835);
+            this.splitContainer1.SplitterDistance = 490;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // buttonLoadData
+            // 
+            this.buttonLoadData.Location = new System.Drawing.Point(17, 16);
+            this.buttonLoadData.Name = "buttonLoadData";
+            this.buttonLoadData.Size = new System.Drawing.Size(126, 40);
+            this.buttonLoadData.TabIndex = 0;
+            this.buttonLoadData.Text = "Load DWD Data";
+            this.buttonLoadData.UseVisualStyleBackColor = true;
+            this.buttonLoadData.Click += new System.EventHandler(this.buttonLoadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1471, 835);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Solar House Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonLoadData;
     }
 }
 
