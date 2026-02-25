686ab26db2f138aed92165cb8b35ddc5f4bb2b44
rt <roman.thiel@web.de>
2026-02-25 18:34:26 +0100

    average added

--- SolarHouseSimulator/Form1.cs
+++ SolarHouseSimulator/Form1.cs
@@ -69,54 +69,106 @@ namespace SolarHouseSimulator
             return combined;
         }
 
+        private double[] CalculateMovingAverage(double[] data, int windowSize)
+        {
+            double[] result = new double[data.Length];
+            double sum = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sum += data[i];
+                if (i >= windowSize)
+                {
+                    sum -= data[i - windowSize];
+                    result[i] = sum / windowSize;
+                }
+                else
+                {
+                    // Während der Aufwärmphase des Fensters
+                    result[i] = sum / (i + 1);
+                }
+            }
+            return result;
+        }
+
         private void RunPvgisSimulationAndPlot(string[] filePaths)
         {
             // 1. Daten laden und summieren
             var solarData = LoadAndSumPvgisFiles(filePaths);
             var sortedTimes = solarData.Keys.OrderBy(t => t).ToList();
 
-            // 2. Simulations-Variablen
-            double batteryWh = (double)numericUpDownBat.Value / 2; // Start 50%
-            double maxBatteryWh = (double)numericUpDownBat.Value;
-
+            // 2. Listen für den Plot (Hier werden sie definiert!)
             List<double> xs = new List<double>();
             List<double> ysSolarKw = new List<double>();
             List<double> ysBatteryPct = new List<double>();
 
+            // Simulations-Variablen
+            double batteryWh = 15000; // Start 50%
+            double maxBatteryWh = 30000;
+
             foreach (var time in sortedTimes)
             {
                 double solarWatt = solarData[time];
-                double loadWatt = GetHouseLoadWatt(time); // Deine Verbrauchsfunktion
+                double loadWatt = GetHouseLoadWatt(time); // Deine Funktion
 
-                // PVGIS sind Stundenwerte -> DeltaWh = Watt * 1 Stunde
+                // Simulation (Stunden-Intervall)
                 double deltaWh = solarWatt - loadWatt;
                 batteryWh += deltaWh;
 
-                // Akku-Grenzen (C# Clamp)
                 if (batteryWh > maxBatteryWh) batteryWh = maxBatteryWh;
                 if (batteryWh < 0) batteryWh = 0;
 
-                // Daten für Plot sammeln
+                // Daten zu Listen hinzufügen
                 xs.Add(time.ToOADate());
                 ysSolarKw.Add(solarWatt / 1000.0);
                 ysBatteryPct.Add((batteryWh / maxBatteryWh) * 100.0);
             }
 
-            // 3. Plotten in ScottPlot 5
+            // 3. Gleitender Mittelwert berechnen (Fenster: 24 Stunden)
+            double[] solarMovingAvg = CalculateMovingAverage(ysSolarKw.ToArray(), 24);
+
+            // 1. Alles Vorherige löschen
             formsPlot.Plot.Clear();
 
-            var solarPlot = formsPlot.Plot.Add.Scatter(xs.ToArray(), ysSolarKw.ToArray());
-            solarPlot.LegendText = "PV Summe (kW)";
-            solarPlot.Color = ScottPlot.Colors.Blue;
+            // 2. Zeit-Achse (X) vorbereiten
+            double[] xsArray = xs.ToArray();
 
-            var batteryPlot = formsPlot.Plot.Add.Scatter(xs.ToArray(), ysBatteryPct.ToArray());
+            // 3. Solar-Kurve (Linke Achse)
+            var solarPlot = formsPlot.Plot.Add.Scatter(xsArray, ysSolarKw.ToArray());
+            solarPlot.LegendText = "PV Leistung (kW)";
+            solarPlot.Color = ScottPlot.Colors.Blue.WithAlpha(0.3);
+            solarPlot.Axes.YAxis = formsPlot.Plot.Axes.Left; // Explizit Links
+
+            // 4. Mittelwert-Kurve (Linke Achse)
+            var avgPlot = formsPlot.Plot.Add.Scatter(xsArray, solarMovingAvg);
+            avgPlot.LegendText = "Tagesmittel (kW)";
+            avgPlot.Color = ScottPlot.Colors.Gold;
+            avgPlot.LineWidth = 2;
+            avgPlot.Axes.YAxis = formsPlot.Plot.Axes.Left; // Explizit Links
+
+            // 5. Akku-Kurve (Rechte Achse)
+            var batteryPlot = formsPlot.Plot.Add.Scatter(xsArray, ysBatteryPct.ToArray());
             batteryPlot.LegendText = "Akku (%)";
             batteryPlot.Color = ScottPlot.Colors.Red;
-            // Optional: Batterie auf die rechte Achse legen
-            batteryPlot.Axes.YAxis = formsPlot.Plot.Axes.Right;
+            batteryPlot.Axes.YAxis = formsPlot.Plot.Axes.Right; // Explizit Rechts
 
+            // 6. ACHSEN-LOGIK (Stabilisiert)
+            // X-Achse auf Zeit umstellen
             formsPlot.Plot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.DateTimeAutomatic();
+
+            // WICHTIG: Erst AutoScale für alle Achsen, dann spezifische Limits
             formsPlot.Plot.Axes.AutoScale();
+
+            // Jetzt nur die RECHTE Achse auf 0-100 pinnen (für die Prozentanzeige)
+            formsPlot.Plot.Axes.Right.Range.Min = 0;
+            formsPlot.Plot.Axes.Right.Range.Max = 105; // Etwas Puffer nach oben
+
+            // Beschriftungen aktivieren
+            formsPlot.Plot.Axes.Left.Label.Text = "PV Leistung [kW]";
+            formsPlot.Plot.Axes.Right.Label.Text = "Akku [%]";
+            // In ScottPlot 5 muss die rechte Achse oft explizit sichtbar geschaltet werden:
+            formsPlot.Plot.Axes.Right.IsVisible = true;
+
             formsPlot.Plot.ShowLegend();
             formsPlot.Refresh();
         }
